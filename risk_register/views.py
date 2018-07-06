ffd70da0c4580576ea59e0176d381ffb0a3dd1dd
justin441 <fotuejustin441@gmail.com>
2018-07-06 16:09:46 +0100

    ajout de django-autocomplete-light

--- risk_register/views.py
+++ risk_register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from fm.views import AjaxCreateView
 
 from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
 
 from risk_management.users.models import BusinessUnit
 from .models import (ActiviteRisque, ProcessusRisque, Processus, Activite)
@@ -51,3 +52,12 @@ class ActiviteRiskRegisterView(DetailView):
 
 class CreateProcessView(AjaxCreateView):
     form_class = CreateProcessForm
+    message_template = 'risk_register/process_card.html'
+
+    def pre_save(self):
+        self.object.business_unit = get_object_or_404(BusinessUnit, denomination=self.kwargs['business_unit'])
+
+    def get_message_template_context(self):
+        msg_ctx = super().get_message_template_context()
+        msg_ctx['processus'] = self.object
+        return msg_ctx
