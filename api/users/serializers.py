04ece84639bd1fea891e8ba394a4e12ccfe16ca1
justin441 <fotuejustin441@gmail.com>
2018-11-23 12:02:13 +0100

    modification de la structure du module api

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
+from risk_register.models import Processus
 from risk_management.users.models import User, BusinessUnit, Position
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'civilite', 'first_name', 'last_name', 'telephone')
+        fields = ('email', 'civilite', 'first_name', 'last_name', 'telephone', 'postes')
 
 
 class BuSerializer(serializers.ModelSerializer):
     class Meta:
         model = BusinessUnit
         fields = ('denomination', 'raison_sociale', 'sigle', 'marche', 'ville_siege', 'adresse_physique', 'projet',
-                  'adresse_postale', 'telephone', 'site_web', 'bu_manager', 'employes', )
+                  'adresse_postale', 'telephone', 'site_web', 'bu_manager', 'employes', 'processus_set')
 
 
 class PositionSerializer(serializers.ModelSerializer):
