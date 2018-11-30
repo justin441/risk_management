20c0909e0ba036934e6334d68e736285238c5e91
justin441 <fotuejustin441@gmail.com>
2018-11-30 10:53:39 +0100

    ajout de vuetify

--- api/users/api_views.py
+++ api/users/api_views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework import authentication
+
+from rest_auth.views import LogoutView
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
@@ -6,26 +10,42 @@ from django.views.decorators.vary import vary_on_cookie
 from . import serializers
 
 from risk_management.users import models
-from rest_framework import viewsets
 
 
-class UserlistView(generics.ListAPIView):
+class UserlistView(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+    """
+    Lister les utilisateurs.
+    """
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
     lookup_field = 'uuid'
 
 
-class UserCreateview(generics.CreateAPIView):
+class UserCreateview(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    """
+    Creer un utilisateur
+    """
     queryset = models.User.objects.all()
     serializer_class = serializers.UserCreationSerializer
 
 
-class BusinessUnitview(viewsets.ModelViewSet):
+class BusinessUnitviewSet(viewsets.ModelViewSet):
+    """
+    Lister, creer, modifier, detailler et supprimer les business unit
+    """
     queryset = models.BusinessUnit.objects.all()
     serializer_class = serializers.BuSerializer
     lookup_field = 'uuid'
 
-    @method_decorator(cache_page(60*60*2))
-    @method_decorator(vary_on_cookie)
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+
+class PositionViewSet(viewsets.ModelViewSet):
+    """
+    Lister, creer, modifier, detailler et supprimer les postes
+    """
+    queryset = models.Position.objects.all()
+    serializer_class = serializers.PositionSerializer
+
+
+class RmLogoutView(LogoutView):
+    authentication_classes = (authentication.TokenAuthentication,)
+
